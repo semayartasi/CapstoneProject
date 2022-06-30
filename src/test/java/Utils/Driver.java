b789097d7ab2b24f0de95b4c6c251c80d1b18dd5
Sema <semayartasi@hotmail.com>
2022-06-30 18:29:27 -0400

    Second Commit

--- src/test/java/Utils/Driver.java
+++ src/test/java/Utils/Driver.java
@@ -6,21 +6,12 @@ import org.openqa.selenium.chrome.ChromeDriver;
 
 public class Driver {
 
-    /**
-     *
-     *      Why we're creating driver class? (Singleton Driver)
-     *
-     *          Because we need to use 1 driver for every class in out project (page classes and step definitions)
-     *
-     *          This concept called "Singleton Driver"
-     *
-     */
 
 
     private static WebDriver driver;
 
     public static WebDriver getDriver() {
-        // if the driver object is equal to null then create a new driver instance
+
         if (driver == null) {
             WebDriverManager.chromedriver().setup();
             driver = new ChromeDriver();
@@ -31,7 +22,7 @@ public class Driver {
     public static void quitDriver() {
         if (driver != null) {
             driver.quit();
-            driver = null; // if we don't have this line next scenario will fail - NoSuchSessionException
+            driver = null;
         }
     }
 
